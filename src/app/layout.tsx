c964f6a823583bd8447a3932ca95c0a8cf3c12d0
Shubhendu Vaid <shubhendu.vaid@bt.com>
2024-03-07 23:15:02 +0000

    added analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 import StickyNavBar from "../components/stickyNavBar";
@@ -26,6 +28,8 @@ export default function RootLayout({
           {children}
           <SimpleFooter />
         </PageContainerComponent>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
